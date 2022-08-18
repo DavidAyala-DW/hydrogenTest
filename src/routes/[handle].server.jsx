4bb80c39228763692c08c32718f9011ab9a26e21
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-08-17 21:31:56 -0500

    First commit

---
+++ src/routes/[handle].server.jsx
@@ -0,0 +1,25 @@
+import client from "../lib/sanity-client";
+import {useQuery} from '@shopify/hydrogen';
+
+
+export default function Index(props) {  
+
+  const {data} = useQuery(['unique', 'key'], async () => {
+    const request = await client.fetch(`*[_type == 'pet']`);
+    return await request;
+  });
+  console.log(props.params.handle);
+  const actualPet = data.some( pet => pet?.name?.toLowerCase() == props?.params?.handle);
+
+  console.log(actualPet);
+
+  return (
+    <div>
+      <h1>{props.params.handle}</h1>
+      <div>
+        
+      </div>
+    </div>    
+
+  );
+}
