74f3e464eccd393697cfad51f4d3e311b7730022
LuisZanabria <luis.zanabria2@unmsm.edu.pe>
2022-08-18 00:40:39 -0500

    add sanity

---
+++ oldlace-viper/src/dashboardConfig.js
@@ -0,0 +1,18 @@
+export default {
+  widgets: [
+      {
+      name: 'netlify',
+      options: {
+        title: 'My Netlify deploys',
+        sites: [
+          {
+            title: 'Sanity Studio',
+            apiId: 'bf9f904c-b488-41c4-94a5-2bda208b92b9',
+            buildHookId: '62fda971ec1678465c3c2963',
+            name: 'hydrogen-test-david',
+          }
+        ]
+      }
+    }
+  ]
+}
